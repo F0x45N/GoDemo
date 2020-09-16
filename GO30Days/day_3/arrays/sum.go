60b9829b478187fdc68bb859a9fb3c440ce2f390
Yotsawang Sorangkoun <yotsawangs@gmail.com>
2020-09-16 15:54:14 +0700

    Add new test, func, refactorize

--- GO30Days/day_3/arrays/sum.go
+++ GO30Days/day_3/arrays/sum.go
@@ -12,6 +12,27 @@ func Sum(numbers []int) int {
     return sum
 }
 
-func SumAll(numbersToSum ...[]int) (sums []int) {
-    return
+// Legacy code
+/*
+func SumAll(numbersToSum ...[]int) []int {
+    lengthOfNumbers := len(numbersToSum)
+    sums := make([]int, lengthOfNumbers)
+
+    for i, numbers := range numbersToSum {
+        sums[i] = Sum(numbers)
+    }
+
+    return sums
+}
+*/
+
+// Refactorize
+
+func SumAll(numbersToSum ...[]int) []int {
+    var sums []int
+    for _, numbers := range numbersToSum {
+        sums = append(sums, Sum(numbers))
+    }
+
+    return sums
 }
