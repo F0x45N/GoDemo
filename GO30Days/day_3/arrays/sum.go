9787076ba216ffb8e0ef5c663167224c305a2637
Yotsawang Sorangkoun <yotsawangs@gmail.com>
2020-09-16 10:58:23 +0700

    move path

---
+++ GO30Days/day_3/arrays/sum.go
@@ -0,0 +1,9 @@
+package main
+
+// Sum: take numbers in arrays and sum it altogether
+// range loops though an arrays by return the value and index
+// blank identifier used to ignore index as in for range loops
+
+func Sum(numbers [5]int) int {
+    return 0
+}
